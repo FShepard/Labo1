28c1d28fbf95d9d7e1d1ba925b28f6800c2b88fd
FShepard <00114618@uca.edu.sv>
2024-03-22 10:57:31 -0600

    Subiendo

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,76 @@
+import java.lang.invoke.CallSite;
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int respuesta = sc.nextInt();
+       Scanner sc = new Scanner(System.in);
+        int respuesta;
+        System.out.println("Que operacion deseas realizar?");
+        System.out.printf("%n1.Sumar %n2. Restar %n3. Multiplicar %n4.Dividir %n5. Salir");
 
-        System.out.print(respuesta);
+        do {
+            respuesta = sc.nextInt();
 
-if (respuesta == 0 ){
-    System.out.println("Tu respuesta es incorrecta");
-}
-else if (respuesta ==1 )
+            System.out.println ("Ingresa tu primer numero");
+            int a = sc.nextInt();
+            System.out.println ("Ingresa tu segundo numero");
+            int b = sc.nextInt();
 
-    do {
-int a, b;
-switch (respuesta) {
-    case 1:
-        System.out.print("caso 1");
-        S
-        break;
+           // float a = 10, b =20;
 
-    default:
-        System.out.print("opcion no valida");
-        break;
-}
+            switch (respuesta) {
+                case 1:
+                    System.out.println("Respuesta: ");
+                    System.out.print (sumar(a, b));
+                    break;
+                case 2:
+                    System.out.println("Respuesta: ");
+                    System.out.print (restar(a, b));
+                    break;
+                case 3:
+                    System.out.println("Respuesta: ");
+                    System.out.print (multiplicar(a, b));
+                    break;
+                case 4:
+                    System.out.println("Respuesta: ");
+                    System.out.print (dividir(a, b));
+                    break;
 
-}
+                case 5:
+                    System.out.print("Saliendo");
+                    break;
 
-System.out.printf("%n1. Imprimiendo %n2. Hola ");
+
+               default:
+                    System.out.print("opcion no valida");
+                    break;
+            }
+
+} while (respuesta !=5);
+
         }
 
-        public static int sumar (int a, int b ) {
-        return a + b;
+        public static float sumar (float a, float b ) {
+             return a + b;
         }
+        public static float multiplicar (float a, float b ){
+        return a * b;
+        }
+        public static float dividir (float a, float b) {
+        return a / b;
+
+    }
+        public static float restar (float a, float b) {
+        return a - b;
+    }
+
+
+
+
+
     }
 
