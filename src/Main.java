2c871af1add8cee95cf30b3171d93095ba511616
FShepard <00114618@uca.edu.sv>
2024-03-22 10:07:54 -0600

    first commit

---
+++ src/Main.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int respuesta = sc.nextInt();
+
+        System.out.print(respuesta);
+
+if (respuesta == 0 ){
+    System.out.println("Tu respuesta es incorrecta");
+}
+else if (respuesta ==1 )
+
+    do {
+int a, b;
+switch (respuesta) {
+    case 1:
+        System.out.print("caso 1");
+        S
+        break;
+
+    default:
+        System.out.print("opcion no valida");
+        break;
+}
+
+}
+
+System.out.printf("%n1. Imprimiendo %n2. Hola ");
+        }
+
+        public static int sumar (int a, int b ) {
+        return a + b;
+        }
+    }
+
